a0fcce1f49fd340be465bfae2a6433c2f10e753d
lesliedey <ldey@yahoo.com>
2016-04-27 15:20:23 -0400

    Rename chapter 1.txt to chapter 1.swift

--- chapter 1.swift
+++ chapter 1.swift
@@ -16,7 +16,7 @@
 import UIKit
 
 var str = "Hello, playground"
-let hello = "Hola, qué tal"
+let hello = "Hola, quÃ© tal"
 
 //Optionals can be nil or the number they store, if any.
 var optionalInteger: Int?
